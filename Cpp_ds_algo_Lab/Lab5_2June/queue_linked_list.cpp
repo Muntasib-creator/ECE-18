87588e2c05e0b1bd66ed2a170c249f5a99a5685a
Muntasib-creator <muntasibchowdhury@gmail.com>
2022-03-05 13:46:41 +0600

    OOP and ds_algo lab

---
+++ Cpp_ds_algo_Lab/Lab5_2June/queue_linked_list.cpp
@@ -0,0 +1,85 @@
+//Applications of Queue
+
+//Serving requests on a single shared resource, like a printer, CPU task scheduling etc.
+//In real life scenario, Call Center phone systems uses Queues to hold people
+//calling them in an order, until a service representative is free.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+};
+
+Node *front;
+Node *rear;
+
+void enQueue(int d)
+{
+    Node* temp = new Node();
+    temp->data = d;
+    temp->next = NULL;
+
+    if(front == NULL)
+    {
+        front = rear = temp;
+    }
+    else
+    {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+void deQueue()
+{
+    if (front == NULL)
+    {
+        cout<<"underflow"<<endl;
+    }
+    else
+    {
+        front = front->next;
+
+        if(front == NULL)
+        {
+            rear = NULL;
+        }
+    }
+}
+void display(){
+    if(front==NULL) return;
+    else{
+        Node* temp = front;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+	enQueue(10);
+	enQueue(20);
+    display();
+
+	deQueue();
+	deQueue();
+    display();
+
+	enQueue(30);
+	enQueue(40);
+	enQueue(50);
+	deQueue();
+    display();
+    
+    enQueue(60);
+
+	cout << "Queue Front : " << front->data << endl;
+	cout << "Queue Rear : " << rear->data;
+}
