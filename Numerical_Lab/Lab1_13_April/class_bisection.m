68d73c8e7e9d0d1e416af4cb62ca111431b286e7
Muntasib-creator <muntasibchowdhury@gmail.com>
2022-04-13 16:32:18 +0600

    Numerical_method lab-1

---
+++ Numerical_Lab/Lab1_13_April/class_bisection.m
@@ -0,0 +1,25 @@
+clc;
+x1=input('Enter initial value x1:')
+x2=input('Enter initial value x2:')
+y1=x1^2-4*x1-10;
+y2=x2^2-4*x2-10;
+y1
+y2
+if(y1*y2>0)
+    display('no roots within x1 & x2')
+else
+    x3=(x1+x2)/2;
+    y3=x3^2-4*x3-10;
+    while abs((x2-x1)/x2)>0.0001
+        if(y1*y3<0)
+            x2=x3;
+            y2=y3;
+        else
+            x1=x3;
+            y1=y3;
+        end;
+        x3=(x1+x2)/2;
+        y3=x3^2-4*x3-10;
+    end;
+    display(x3);
+end;
